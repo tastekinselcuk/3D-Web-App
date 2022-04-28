c79fa710f4b937dfd78b8ccafa5c0180b40c2339
Selcuk2534 <77862040+tastekinselcuk@users.noreply.github.com>
2022-04-28 18:04:06 +0300

    files added

---
+++ js/cdnThreee.js
@@ -0,0 +1,5 @@
+
+            // Find the latest version by visiting https://cdn.skypack.dev/three.
+          
+            import * as THREE from 'https://cdn.skypack.dev/three@<version>';
+          
